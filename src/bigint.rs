1b7b12023b4bb5a89f705dd930b99bce3ef46274
kimuti-tsukai <ruster354@gmail.com>
2024-08-19 03:07:23 +0900

    Impl constructors for BigInt

--- src/bigint.rs
+++ src/bigint.rs
@@ -1,7 +1,82 @@
 use crate::BigUint;
 
-#[derive(Clone, Debug, PartialEq, Eq, Hash)]
+#[derive(Clone, Copy, Debug, Hash, PartialEq, Eq)]
+enum Sign {
+    Positive = 1,
+    Zero = 0,
+    Negative = -1
+}
+
+impl Default for Sign {
+    fn default() -> Self {
+        Self::Positive
+    }
+}
+
+#[derive(Clone, Debug, Hash, Default, PartialEq, Eq)]
 pub struct BigInt {
-    sign: bool,
+    sign: Sign,
     value: BigUint
+}
+
+// Private methods
+impl BigInt {
+    fn is_zero_and_set_sign(&mut self) {
+        if self.is_zero() {
+            self.sign = Sign::Zero
+        }
+    }
+
+    fn is_zero_and_sign(mut self) -> Self {
+        if self.is_zero() {
+            self.is_zero_and_set_sign();
+        }
+
+        self
+    }
+}
+
+// Public methods
+impl BigInt {
+    pub fn new() -> Self {
+        BigInt {
+            sign: Sign::Zero,
+            value: BigUint::new()
+        }
+    }
+
+    pub fn zero() -> Self {
+        Self::new()
+    }
+
+    pub fn is_zero(&self) -> bool {
+        self.value.is_zero()
+    }
+
+    pub fn set_zero(&mut self) {
+        *self = Self::zero()
+    }
+
+    pub fn one() -> Self {
+        BigInt {
+            sign: Sign::Positive,
+            value: BigUint::one()
+        }
+    }
+
+    pub fn is_one(&self) -> bool {
+        self == &Self::one()
+    }
+
+    pub fn set_one(&mut self) {
+        *self = Self::one()
+    }
+
+    pub fn is_positive(&self) -> bool {
+        self.sign == Sign::Positive
+    }
+
+    pub fn is_negative(&self) -> bool {
+        self.sign == Sign::Negative
+    }
 }
