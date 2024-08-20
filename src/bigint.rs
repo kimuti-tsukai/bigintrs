0e69e7720fffbfb7aabffadf0c2923d734233294
kimuti-tsukai <ruster354@gmail.com>
2024-08-20 15:55:02 +0900

    Impl Rem

--- src/bigint.rs
+++ src/bigint.rs
@@ -1,6 +1,72 @@
-use std::ops::{Add, Div, Mul, Neg, Sub};
+use std::ops::{Add, Div, Mul, Neg, Rem, Sub};
 
 use crate::BigUint;
+
+macro_rules! impl_for_ref_to_ref {
+    ($trait: ty, $method: ident) => {
+        impl $trait for &BigInt {
+            type Output = BigInt;
+
+            fn $method(self, rhs: Self) -> Self::Output {
+                self.clone().$method(rhs.clone())
+            }
+        }
+    };
+    ($($trait: ty, $method: ident);+) => {
+        $(
+            impl_for_ref_to_ref!($trait, $method);
+        )+
+    };
+}
+
+macro_rules! impl_for_owned_to_ref {
+    ($trait: tt, $method: ident) => {
+        impl $trait<&Self> for BigInt {
+            type Output = BigInt;
+
+            fn $method(self, rhs: &Self) -> Self::Output {
+                self.$method(rhs.clone())
+            }
+        }
+    };
+    ($($trait: tt, $method: ident);+) => {
+        $(
+            impl_for_owned_to_ref!($trait, $method);
+        )+
+    };
+}
+
+macro_rules! impl_for_ref_to_owned {
+    ($trait: tt, $method: ident) => {
+        impl $trait<BigInt> for &BigInt {
+            type Output = BigInt;
+
+            fn $method(self, rhs: BigInt) -> Self::Output {
+                self.clone().$method(rhs)
+            }
+        }
+    };
+    ($($trait: tt, $method: ident);+) => {
+        $(
+            impl_for_ref_to_owned!($trait, $method);
+        )+
+    };
+}
+
+macro_rules! impl_assign_for_ref {
+    ($trait: tt, $method: ident) => {
+        impl $trait<&Self> for BigInt {
+            fn $method(&mut self, rhs: &Self) {
+                self.$method(rhs.clone())
+            }
+        }
+    };
+    ($($trait: tt, $method: ident);+) => {
+        $(
+            impl_assign_for_ref!($trait, $method);
+        )+
+    };
+}
 
 #[derive(Clone, Copy, Debug, Hash, PartialEq, Eq)]
 enum Sign {
@@ -203,3 +269,35 @@ impl Div for BigInt {
         }
     }
 }
+
+impl Rem for BigInt {
+    type Output = Self;
+
+    fn rem(self, rhs: Self) -> Self::Output {
+        &self - (&self / &rhs) * &rhs
+    }
+}
+
+impl_for_ref_to_ref!(
+    Add, add;
+    Sub, sub;
+    Mul, mul;
+    Div, div;
+    Rem, rem
+);
+
+impl_for_owned_to_ref!(
+    Add, add;
+    Sub, sub;
+    Mul, mul;
+    Div, div;
+    Rem, rem
+);
+
+impl_for_ref_to_owned!(
+    Add, add;
+    Sub, sub;
+    Mul, mul;
+    Div, div;
+    Rem, rem
+);
